50a0f982763650ddbf14d2c7ac5b64fde615f3aa
Krishnendu <krishnendunandi1990@gmail.com>
2022-11-26 00:18:13 +0530

    initial commit

---
+++ src/test/java/features/SampleTest.feature
@@ -0,0 +1,7 @@
+@sample-feature
+
+  Feature: Verify sample positive features
+
+    Scenario:Verify postive sample 1
+      Given I hit sample web application
+      Then I verify sample test
---
+++ src/test/java/stepdefs/SampleTestStepDef.java
@@ -0,0 +1,17 @@
+package stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class SampleTestStepDef {
+
+    @Given("I hit sample web application")
+    public void launchApplication(){
+        System.out.println("launching");
+    }
+
+    @Then("I verify sample test")
+    public void verifyTest(){
+        System.out.println("verifying");
+    }
+}
